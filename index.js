ae72ba9ab9dcd95ae7c52ccbb126a354a8511186
christian julius guillen <juliusguillen@gmail.com>
2022-03-09 08:57:36 -0600

    commit tree f760c924da8530ea97e53fc97e1bb1553be623a5

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+
+ function returnsANamedFunction(){
+    return returnFirstName
+}
+function returnFirstName(){
+
+}
+function returnsAnAnonymousFunction() {
+    return function(){};
+}
+function AnAnonymous() {}
+
+
+
+
+
+    
